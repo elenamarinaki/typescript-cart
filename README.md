4ad4f7bf72dd9f69387210becd0111a7a32a05d1
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-16 20:33:59 +0000

    fetching all products, using the useQuery, handling loading & errors

--- README.md
+++ README.md
@@ -6,7 +6,11 @@
   - `npx create-react-app typescript-cart --template typescript`
 - Install `material-ui`
   - `npm i @material-ui/core @material-ui/icons`
+    ⚠️ we cannot use `<React.StrictMode>` with material-ui
 - React Query
   - `npm i react-query`
+
+> Fetch, cache and update data in your React and React Native applications all without touching any "global state".
+
 - Styled-components with Typescript
   - `npm i styled-components @types/styled-components`
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-const App = () => {
-  return <div className="App">Hello!</div>;
-};
+import { useState } from "react"
+import { useQuery } from "react-query"
+// Components
+import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core"
+import { AddShoppingCart } from "@material-ui/icons"
+// Styles
+import { Wrapper } from "./App.styles"
+// Types
+export type CartItemType = {
+  id: number
+  category: string
+  description: string
+  image: string
+  price: number
+  title: string
+  amount: number
+}
 
-export default App;
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json()
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  )
+  console.log(data)
+
+  const getTotalItems = () => null
+  const handleAddToCart = () => null
+  const handleRemoveFromCart = () => null
+
+  if (isLoading) return <LinearProgress />
+  if (error) return <div>Something went wrong ... 🤨</div>
+
+  return <div className='App'>Hello!</div>
+}
+
+export default App
