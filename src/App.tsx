ef98017a6f9ad03b778c0e4f5cdea580b3dd3819
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-16 02:55:08 +0000

    fixed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+const App = () => {
   return <div className="App">Hello!</div>;
-}
+};
 
 export default App;
