2da078cf639ce2ac775de0b9cffb8b9db060b668
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-22 23:10:35 +0000

    mapping the cart items in the cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,1 +1,9 @@
 import { Button } from "@material-ui/core"
+// Types
+import { CartItemType } from "../App"
+// Styles
+import { Wrapper } from "./CartItem.styles"
+
+const CartItem: React.FC = () => <div>Cart Item</div>
+
+export default CartItem
