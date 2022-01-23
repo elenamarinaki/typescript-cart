58dbcc5062e648763905f53efd1677137eba6582
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-23 00:10:32 +0000

    created the +/- buttons for the items in the cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@material-ui/core"
 // Types
 import { CartItemType } from "../App"
+import Item from "../Item/Item"
 // Styles
 import { Wrapper } from "./CartItem.styles"
 
@@ -10,6 +11,36 @@ type Props = {
   removeFromCart: (id: number) => void
 }
 
-const CartItem: React.FC<Props> = () => <div>Cart Item</div>
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className='information'>
+        <p>Price: £{item.price}</p>
+        <p>Total: £{(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <div className='buttons'>
+        <Button
+          size='small'
+          disableElevation
+          variant='contained'
+          onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </Button>
+        <p>{item.amount}</p>
+
+        <Button
+          size='small'
+          disableElevation
+          variant='contained'
+          onClick={() => addToCart(item)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+  </Wrapper>
+)
 
 export default CartItem
